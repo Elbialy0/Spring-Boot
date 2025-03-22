8b54fc7f7c519c79525737b95cb469e2ff37c071
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2025-03-22 18:39:28 +0200

    Finish the download endpoint

--- fileManager/src/main/java/com/elbialy/fileManager/FileManagerController.java
+++ fileManager/src/main/java/com/elbialy/fileManager/FileManagerController.java
@@ -1,23 +1,43 @@
 package com.elbialy.fileManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
 
 @RestController
 public class FileManagerController {
     @Autowired
     private FileManagerService fileManagerService;
     @PostMapping("/upload")
-    public void upload(@RequestParam MultipartFile file) {
+    public void upload(@RequestParam("file") MultipartFile file) {
         try {
             fileManagerService.upload(file);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+
+    }
+    @GetMapping("/download")
+    public ResponseEntity<Resource> download(@RequestParam("fileName") String fileName) {
+        try {
+            var file = fileManagerService.getFile(fileName);
+            return ResponseEntity.ok().contentLength(file.length())
+                    .header("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"")
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(new InputStreamResource(Files.newInputStream(file.toPath())));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
 
     }
 }
--- fileManager/src/main/java/com/elbialy/fileManager/FileManagerService.java
+++ fileManager/src/main/java/com/elbialy/fileManager/FileManagerService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -24,5 +25,18 @@ public class FileManagerService {
         Files.copy(file.getInputStream(),savedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
         // if the file is present it will replace it
 
+    }
+    public File getFile(String fileName) throws FileNotFoundException {
+        if (null == fileName){
+            throw new IllegalArgumentException("file name is null");
+        }
+        var file = new File(UPLOAD_DIR + File.separator + fileName);
+        if(!Objects.equals(file.getParent(), UPLOAD_DIR)){
+            throw new SecurityException("Unsafe file path");
+        }
+        if (!file.exists()){
+            throw new FileNotFoundException("file not found");
+        }
+        return file;
     }
 }
