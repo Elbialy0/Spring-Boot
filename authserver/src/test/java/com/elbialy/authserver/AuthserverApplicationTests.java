83b1d1e859ac866e0a7919dcf9c3591a788b9e7d
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2025-03-27 19:07:29 +0200

    Auth Server

---
+++ authserver/src/test/java/com/elbialy/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbialy.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
